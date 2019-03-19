9084ef8b609c0ecf789423b6cf686b7c982ef51a
Anna Nikulasdottir <anna.b.nik@gmx.de>
2019-03-19 18:55:53 +0000

    add tag mode and test mode

--- normalizing/normalizer.py
+++ normalizing/normalizer.py
@@ -20,25 +20,28 @@ References:
     Ebden, Peter and Sproat, Richard. 2015. The Kestrel TTS text normalization
     system. Natural Language Engineering, Issue 03, pp 333-353.
 
-TODO: make a classification only procedure possible
 
 """
 
 import os
 import configparser
+from timeit import default_timer as timer
 import pynini as pn
+import nlp
 from fst_parser import FSTParser
 from tokenizer import Tokenizer
 from classifier import Classifier
 from verbalizer import Verbalizer
 from utterance_structure.utt_coll import UtteranceCollection
 from utterance_structure.utt_coll import Utterance
+from utterance_structure.utt_coll import TokenType
 
 
 class Normalizer:
 
-    def __init__(self, configfile='normalizer.conf', working_dir=None, verbalize=True):
+    def __init__(self, configfile='normalizer.conf', working_dir=None, verbalize=True, test_mode=False, tag_mode=False):
 
+        #TODO: print out info on used language model/grammar mode/test mode
         if working_dir:
             current_dir = working_dir
         else:
@@ -64,6 +67,8 @@ class Normalizer:
         if verbalize:
             self.verbalizer = Verbalizer(verbalizer_grammar_file, lm_file, self.utf8_symbols, word_symbols)
         self.utterance_collection = None
+        self.test_mode = test_mode
+        self.tag_mode = tag_mode
 
 
     def normalize(self, text):
@@ -77,11 +82,14 @@ class Normalizer:
         normalized_text = []
         sentence_list = self.tok.tokenize_sentence(text)
         for sent in sentence_list:
+            print("processing '" + sent + "' ...")
             self.utterance_collection.add_utterance(Utterance(sent))
         for utt in self.utterance_collection.collection:
             self._normalize_utterance(utt)
-            if self.verbalize:
+            if self.verbalize and not self.test_mode:
                 normalized_text.append(utt.normalized_sentence)
+            elif self.test_mode:
+                normalized_text.append(utt.get_test_output())
             else:
                 normalized_text.append(utt.classified)
 
@@ -107,6 +115,8 @@ class Normalizer:
         utt.tokenized = self.tok.tokenize_words(utt.original_sentence)
         utt.tokenized_string = ' '.join(utt.tokenized)
         classified_fst = self._classify(utt)
+        if not utt.classified:
+            return
         if self.verbalize:
             self._verbalize(classified_fst, utt)
 
@@ -134,7 +144,28 @@ class Normalizer:
     def _verbalize(self, classified_fst, utt):
         parser = FSTParser(self.utf8_symbols)
         parser.parse_tokens_from_fst(classified_fst, utt)
+        if self.tag_mode:
+            self._tag_utterance(utt)
         self.verbalizer.verbalize(utt)
+
+
+    def _tag_utterance(self, utt):
+        #tagged = nlp.tag_sentence(utt.original_sentence)
+        token_arr = []
+        for tok in utt.ling_structure.tokens:
+            token_arr.append(tok.word)
+        tokens = '\n'.join(token_arr)
+        tagged = nlp.tag_sentence(tokens)
+        print(tagged)
+        tagged_arr = tagged.split()
+        tag_ind = 0
+        for ind, tok in enumerate(utt.ling_structure.tokens):
+            if tok.token_type != TokenType.SEMIOTIC_CLASS:
+                tagged_token = tagged_arr[tag_ind]
+                while tagged_token[:tagged_token.index('_')].lower() != tok.word.lower():
+                    tag_ind += 1
+                    tagged_token = tagged_arr[tag_ind]
+                tok.word = tagged_arr[tag_ind]
 
 
 
@@ -157,14 +188,46 @@ class Normalizer:
 
 
 def main():
-
-    input_text = "Afkoma ársins 2017 var góð. Hagnaður fyrir óinnleysta fjármagnsliði hefur aldrei verið meiri. " \
+    import sys
+    start = timer()
+    #input_text = "Afkoma ársins 2017 var góð."
+    input_text_x = "Afkoma ársins 2017 var góð. Hagnaður fyrir óinnleysta fjármagnsliði hefur aldrei verið meiri. " \
                  "Tekjur voru meiri en nokkru sinni fyrr og sett voru met í orkuvinnslu og -sölu á árinu. " \
-                 "Selt magn var 14,3 teravattstundir og jókst um 5,1% milli ára. "
+                 "Selt magn var 14,3 twst og jókst um 5,1% milli ára. " \
+                 "DNA-rannsóknir eða rannsóknir á kjarnasýrunum."
 
-    norm = Normalizer(verbalize=False)
-    normalized = norm.normalize(input_text)
-    print(normalized)
+
+    inp = 'test_from_training_data_visir201303.txt'
+    output = '../evaluation/eval_results_final/training_test_MIXED.txt'
+    input_text = open(inp).read().splitlines()
+    norm = Normalizer(test_mode=True, tag_mode=False)
+    #inp = ' Bryndís Rún Hansen varð annar Íslendingurinn á HM í 25 m laug í Kanada til að komast í undanúrslit í sinni grein en það gerði hún í 50 m flugsundi nú síðdegis .'
+    #inp = 'Um 2 prósent nefna og 3 prósent'
+    #normalized = norm.normalize(inp)
+    #print(normalized)
+    #exit()
+    all_normalized = []
+    for line in input_text:
+        normalized = norm.normalize(line)
+        #whole lines, original + normalization
+        #all_normalized.append(line + '\t' + normalized)
+        #test output, token-by-token
+        all_normalized.append(normalized)
+        #if len(all_normalized)%500 == 0:
+        #    with open(output, 'a') as f:
+        #        for sent in all_normalized:
+        #            if sent != "I'm normalized":
+        #                f.write(sent + '\n')
+        #    all_normalized = []
+
+    end = timer()
+    print('Total duration: ' + str(end - start))
+
+    with open(output, 'w') as f:
+        for norm in all_normalized:
+            if norm != "I'm normalized":
+                f.write(norm + '\n')
+
     #norm.print_normalized_text()
 
     #with open("utterance.json") as read_file:
