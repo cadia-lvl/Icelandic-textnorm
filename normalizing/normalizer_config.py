cf6082f5c9504e7c2ab5e7c0f4da10a4b2e4cbcd
Anna Nikulasdottir <anna.b.nik@gmx.de>
2019-12-10 15:59:36 +0000

    config to mixed model

--- normalizing/normalizer_config.py
+++ normalizing/normalizer_config.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Creates the configuration file needed by text normalization module expander.py
+Creates the configuration file needed by text normalization module normalizer.py
 
 Place your utf8-symbol file, word-symbol file, grammar-fst and language model in one directory
 (e.g. 'data/'), change the value sides of this config dictionary according to your filenames.
@@ -15,13 +15,13 @@ config = configparser.ConfigParser()
 config['DATA_DIR'] = {'data': 'data/'}
 config['symbol tables'] = {}
 config['symbol tables']['utf8'] = 'utf8.syms'
-config['symbol tables']['word-symbol'] = 'gigacorpus_numbers_raw_word_sym.txt'
+config['symbol tables']['word-symbol'] = 'mixed_word_sym.txt'
 config['models'] = {}
-config['models']['language model'] = 'gigacorpus_numbers_raw_for_lm_unk.fst'
+config['models']['language model'] = 'TRAINING_MIXED_for_lm_unk.fst'
 config['thrax'] = {'thrax': 'thrax_grammar/'}
 config['thrax grammars'] = {}
 config['thrax grammars']['classifier grammar'] = 'classify/TOKENIZE_AND_CLASSIFY'
-config['thrax grammars']['verbalizer grammar'] = 'verbalize/ALL'
+config['thrax grammars']['verbalizer grammar'] = 'verbalize_tags/ALL'
 
 with open('normalizer.conf', 'w') as configfile:
     config.write(configfile)
